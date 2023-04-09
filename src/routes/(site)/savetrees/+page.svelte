564f5a77375499b1db72820f18c45df44c20e9fb
Hyudini <303940abc@gmail.com>
2023-04-09 12:01:43 -0700

    ola finished up savetrees

--- src/routes/(site)/savetrees/+page.svelte
+++ src/routes/(site)/savetrees/+page.svelte
@@ -8,17 +8,179 @@
     */
   </script>
   
-<Center>
-    <div class= "welcome"> 
-    HSAVE TREEES
-    </div>  
-</Center>
+  <section id="latest">
+    <ul>
+      <li class="tree-items">
+        <article>
+          <img src="src/routes/(site)/savetrees/Rectangle9.png" alt="rec">
+          <div class="tree-items-container">
+            <h2>
+                HELP TREES
+            </h2>
+            <a href="https://www.greenamerica.org/save-trees" class="btn">Learn More</a>
+          </div>
+        </article>
+      </li>
+      <li class="tree-items">
+        <article>
+          <img src="src/routes/(site)/savetrees/Rectangle10.png" alt="rec">
+          <h2>
+            HELP TREES
+        </h2>
+        <a href="https://shop.arborday.org/nursery?_gl=1*glm8yg*_ga*MTUyNDQxNDQwMi4xNjgwOTk4MTE5*_ga_S539C3X6HH*MTY4MTA2MzE2OC41LjEuMTY4MTA2MzQ4Mi42MC4wLjA." class="btn">Learn More</a>
+        </article>
+        </li>
+        <li class="tree-items">
+            <article>
+              <img src="src/routes/(site)/savetrees/Rectangle11.png" alt="rec">
+              <div class="tree-items-container">
+                <h2>
+                    HELP TREES
+                </h2>
+                <a href="https://www.edenprojects.org/" class="btn">Learn More</a>
+              </div>
+            </article>
+            </li>
+            <li class="tree-items">
+                <article>
+                  <img src="src/routes/(site)/savetrees/Rectangle12.png" alt="rec">
+                  <div class="tree-items-container">
+                    <h2>
+                        HELP TREES
+                    </h2>
+                    <a href="https://trees.org/" class="btn">Learn More</a>
+                  </div>
+                </article>
+                </li>
+            </ul>
+
+</section>
+    <section id="latest-section">
+        <ul>
+            <li class="tree-items">
+                <article>
+                    <img src="src/routes/(site)/savetrees/Rectangle12.png" alt="rec">
+                    <div class="tree-items-container">
+                      <h2>
+                          HELP TREES
+                      </h2>
+                      <a href="https://www.internationaltreefoundation.org/" class="btn">Learn More</a>
+                    </div>
+                  </article>
+            </li>
+            <li class="tree-items">
+                <article>
+                  <img src="src/routes/(site)/savetrees/Rectangle13.png" alt="rec">
+                  <div class="tree-items-container">
+                    <h2>
+                        HELP TREES
+                    </h2>
+                    <a href="https://www.treepeople.org/" class="btn">Learn More</a>
+                  </div>
+                </article>
+            </li>
+            <li class="tree-items">
+                <article>
+                  <img src="src/routes/(site)/savetrees/Rectangle10.png" alt="rec">
+                  <div class="tree-items-container">
+                    <h2>
+                        HELP TREES
+                    </h2>
+                    <a href="https://onetreeplanted.org/" class="btn">Learn More</a>
+                  </div>
+                </article>
+            </li>
+            <li class="tree-items">
+                <article>
+                  <img src="src/routes/(site)/savetrees/Rectangle11.png" alt="rec">
+                  <div class="tree-items-container">
+                    <h2>
+                        HELP TREES
+                    </h2>
+                    <a href="https://treeswaterpeople.org/" class="btn">Learn More</a>
+                  </div>
+                </article>
+            </li>
+        </ul>
+    </section>
+
 
 
 <style>
-    .welcome{
-    color: #000000;
-    font-size: x-large;
-    font-weight: 500;
+   #latest {
+    display: flex;
+    padding: 10rem 5rem 5;
+    width: 30%;
+    /* top right bottom left  */
+    margin: 3rem 3rem 2rem 1em ;
+
+}
+   #latest ul {
+   display: flex;
+    /* grid-template-columns: 1fr 1fr 1fr 1fr; */
+    gap: 1rem;
+    margin: 0;
+    white-space: pre-wrap;
+    list-style: none;
+    
+   }
+   #latest-section {
+    display: flex;
+    padding: 5rem 4rem 5;
+    width: 30%;
+    /* top right bottom left  */
+    margin: 3rem 3rem 2rem 1rem ;
+   }
+
+   #latest-section ul {
+   display: flex;
+    /* grid-template-columns: 1fr 1fr 1fr 1fr; */
+    gap: 1rem;
+    margin: 0;
+    white-space: pre-wrap;
+    list-style: none;
+    
+   }
+  
+   .tree-items {
+    background-color: #D9D9D9;
+    border-radius: 10px;
+    border: #0d4d0f solid 5px;
+    overflow: hidden;
+    padding-bottom: 10px;
+   }
+    .tree-items img {
+    height: 10rem ;
+    width: 20rem;
+    /* height: 100%; */
+     object-fit: cover;
     }
+    .tree-item h2 {
+    margin: 0 0 1rem 0;
+    color: #fff;
+    }
+
+    .tree-items-container{
+        padding: 0.5rem;
+        text-align: left;
+    }
+    .btn {
+    background-color: #3C6255;
+    color: white;
+    /* border radius can be left in px  */
+    border-radius: 4px;
+    border: solid 1px #0d4d0f;
+    padding: 0.5rem 1.5rem;
+    transition: all 0.3s ease;
+}
+
+    .btn:hover {
+        background-color: #0d4d0f;
+        color: white;
+        box-shadow: 2px 2px 5px #888888;
+        transform: scale(1.1);
+        
+    }
+
+  
 </style>
