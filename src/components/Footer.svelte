e1f5135656a654940e78d49ab3f83ee457dd32f3
Adam101k <adamkaci3@csu.fullerton.edu>
2023-04-08 22:02:04 -0700

    It works?

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,14 @@
 <footer>
-    <div class="copyright">Copyright 2023. All Rights Reserved</div>
+    <div class="copyright">Copyright 2023 WowTrees. All Rights Reserved</div>
 </footer>
 
 <style>
     footer {
         padding: 40px;
         text-align: center;
+        position: fixed; 
+        bottom:0%;
+        width:100%;
         
     }
 
@@ -13,6 +16,7 @@
         color: #0c0c0c;
         font-size: 14px;
         display: inline-block;
+
         padding: 20px;
         border-top: 1px solid #033503;
     }
