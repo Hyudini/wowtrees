34c314ac9eef76f98405f925a7f36caae75715e3
Hyudini <303940abc@gmail.com>
2023-04-09 02:48:05 -0700

    ho

--- src/routes/(site)/trees/+page.svelte
+++ src/routes/(site)/trees/+page.svelte
@@ -1,43 +1,49 @@
-<script>
-    import { onMount } from "svelte";
-    import { apiData, drinkNames } from './store.js';
-    
-    const options = {
-	method: 'GET',
-	headers: {
-		Authorization: 'GKZOHNZj0xP65kk0BAE2Tl9LGagm0pfD3DFNxAEEZcMQBhRZVDco8vbNJdnwwCo0',
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-		'X-RapidAPI-Host': 'plants2.p.rapidapi.com'
-	}
-    };
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import axios from 'axios';
+    let searchTerm = '';
+    let results: any[] = [];
+    // let results = [];
+    async function searchPlants(searchTerm: string) {
+        try {
+            const response = await axios.get('https://trefle.io/api/v1/plants', {
+            params: {
+                q: searchTerm,
+                token: '9da831380emshb347c50d8eb3dfdp1572bejsncd79f183eb82'
+            }
+            });
+            const plants = response.data.data;
+            const results = plants.map(plant => {
+            return {
+                commonName: plant.common_name,
+                scientificName: plant.scientific_name
+            };
+            });
+            return results;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     onMount(async () => {
-        fetch('https://plants2.p.rapidapi.com/api/plants?id=63ef4eb7476230641c4c0d62', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+      results = await searchPlants(searchTerm);
     });
-    </script>
-    
-    <div class="trees">
-        <h1>Trees</h1>
-        <p>
-            <input id="commonname">
-            <br/>
-            <button id="submit">submit</button>
-        </p>
-        <ul>
-        {#each $drinkNames as drinkName}
-            <li>{drinkName}</li>
-        {/each}
-        </ul>
-    </div>
+  </script>
+  <form on:submit|preventDefault={async () => {
+    results = await searchPlants(searchTerm);
+  }}>
+    <label for="searchTerm">Search:</label>
+    <input type="text" id="searchTerm" bind:value={searchTerm}>
+    <button type="submit">Go</button>
+  </form>
+  {#if results.length > 0}
+  <ul>
+    {#each results as result}
+      <li>
+        <h3>{result.commonName}</h3>
+        <p>{result.scientificName}</p>
+      </li>
+    {/each}
+  </ul>
+{/if}
 
-
-<style>
-    .trees{
-    color: #000000;
-    font-size: x-large;
-    font-weight: 500;
-    }
-</style>
